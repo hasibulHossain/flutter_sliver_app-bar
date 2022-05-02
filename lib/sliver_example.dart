a2031028de282744f72a227ee36e4347ea3f1c71
Hasibul Hossain <hasibulhossain52@gmail.com>
2022-05-03 00:48:20 +0600

    added slivers

--- lib/sliver_example.dart
+++ lib/sliver_example.dart
@@ -11,6 +11,39 @@ class SliverExample extends StatelessWidget {
         SliverAppBar(
           title: Text("Horizons"),
           backgroundColor: Colors.teal[800],
+          pinned: true,
+          floating: true,
+          stretch: true,
+          onStretchTrigger: () async {
+            print('stretched');
+          },
+          // snap: true,
+          expandedHeight: 200.0,
+          flexibleSpace: FlexibleSpaceBar(
+            stretchModes: const <StretchMode>[
+              StretchMode.blurBackground,
+              StretchMode.zoomBackground,
+              StretchMode.fadeTitle,
+            ],
+            title: Text('Horizons'),
+            background: DecoratedBox(
+              position: DecorationPosition.foreground,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.center,
+                  colors: [
+                    Colors.teal[800]!,
+                    Colors.transparent,
+                  ],
+                ),
+              ),
+              child: Image.network(
+                'https://images.unsplash.com/photo-1651419935061-c7f954d37446?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
         ),
         MainList(),
       ],
@@ -22,7 +55,7 @@ class MainList extends StatelessWidget {
   MainList({Key? key}) : super(key: key);
 
   final List<int> list =
-      List.generate(14, (int index) => index * index, growable: false);
+      List.generate(24, (int index) => index * index, growable: false);
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +72,7 @@ class MainList extends StatelessWidget {
             ),
           );
         },
-        childCount: 14,
+        childCount: 24,
       ),
     );
   }
